569d53b0b217fbe52b1037d76a5b97a7abdf5362
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2024-09-12 20:15:24 +0930

    replaced findviewbyid to viewbinding

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -7,21 +7,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.tipscalculator.databinding.ActivityMainBinding
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btnClear: Button = findViewById(R.id.btn_clear)
-        val btnCalculate: Button = findViewById(R.id.btn_calculate)
-        val edtTotal: TextInputEditText = findViewById(R.id.tie_total)
-        val edtNumPeople: TextInputEditText = findViewById(R.id.tie_num_people)
 
-        btnClear.setOnClickListener {
+        binding.btnClear.setOnClickListener {
 
         }
     }
