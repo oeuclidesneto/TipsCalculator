2e306d33e0a3dc9daca338a0491a52f19d60ef6a
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2024-09-16 20:37:39 +0930

    implemented spinner for number of people

--- app/src/main/java/com/example/tipscalculator/MainActivity.kt
+++ app/src/main/java/com/example/tipscalculator/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.tipscalculator
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,55 +23,76 @@ class MainActivity : AppCompatActivity() {
 
         var percentage: Int = 0
         binding.rbOptionOne.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked){
+            if (isChecked) {
                 percentage = 10
             }
         }
 
         binding.rbOptionTwo.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked){
+            if (isChecked) {
                 percentage = 15
             }
         }
 
         binding.rbOptionThree.setOnCheckedChangeListener { _, isChecked ->
-            if(isChecked) {
+            if (isChecked) {
                 percentage = 20
             }
         }
 
-        binding.btnClear.setOnClickListener {
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.num_people,
+            android.R.layout.simple_spinner_item
+        )
 
-        }
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        binding.spinnerNumberOfPeople.adapter = adapter
 
-        binding.btnCalculate.setOnClickListener {
-            val totalTableTemp = binding.tieTotal.text
-            val nPeopleTemp = binding.tieNumPeople.text
+        var numOfPeopleSelected = 0
+        binding.spinnerNumberOfPeople.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                        numOfPeopleSelected = position
+                    }
+                        override fun onNothingSelected(parent: AdapterView<*>?) {
 
-            if (totalTableTemp?.isEmpty() == true ||
-                nPeopleTemp?.isEmpty() == true
-            ) {
-                Snackbar
-                    .make(binding.tieTotal, "Fill out all the fields", Snackbar.LENGTH_LONG)
-                    .show()
-            } else {
-                val totalTable: Float = binding.tieTotal.text.toString().toFloat()
-                val nPeople: Int = nPeopleTemp.toString().toInt()
+                        }
+                    }
 
-                val totalTemp = totalTable / nPeople
-                val tips = totalTemp * percentage / 100
-                val totalWithTips = totalTemp + tips
-                binding.tvResult.text = "Total with tips:   $totalWithTips"
+                    binding.btnCalculate.setOnClickListener {
+                        val totalTableTemp = binding.tieTotal.text
+                        if (totalTableTemp?.isEmpty() == true
+                        ) {
+                            Snackbar
+                                .make(
+                                    binding.tieTotal,
+                                    "Fill out all the fields",
+                                    Snackbar.LENGTH_LONG
+                                )
+                                .show()
+                        } else {
+                            val totalTable: Float = binding.tieTotal.text.toString().toFloat()
+                            val nPeople: Int = numOfPeopleSelected
 
+                            val totalTemp = totalTable / nPeople
+                            val tips = totalTemp * percentage / 100
+                            val totalWithTips = totalTemp + tips
+                            binding.tvResult.text = "Total with tips:   $totalWithTips"
+
+                        }
+                        binding.btnClear.setOnClickListener {
+                            binding.tvResult.text = ""
+                            binding.tieTotal.setText("")
+                            binding.rbOptionOne.isChecked = false
+                            binding.rbOptionTwo.isChecked = false
+                            binding.rbOptionThree.isChecked = false
+                        }
+                    }
+                }
             }
-            binding.btnClear.setOnClickListener {
-                binding.tvResult.text = ""
-                binding.tieTotal.setText("")
-                binding.tieNumPeople.setText("")
-                binding.rbOptionOne.isChecked = false
-                binding.rbOptionTwo.isChecked = false
-                binding.rbOptionThree.isChecked = false
-            }
-        }
-    }
-}
