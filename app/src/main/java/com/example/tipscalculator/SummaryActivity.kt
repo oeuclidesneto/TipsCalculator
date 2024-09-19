2cf1cba4957a513a48fe41a15613b872ebc0bf17
Euclides da Costa Rego Neto <euclides.neto@msn.com>
2024-09-19 23:39:12 +0930

    showing the result at the summary screen

--- app/src/main/java/com/example/tipscalculator/SummaryActivity.kt
+++ app/src/main/java/com/example/tipscalculator/SummaryActivity.kt
@@ -10,5 +10,14 @@ class SummaryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_summary)
+
+        val totalTable = intent.getFloatExtra("totalTable", 0.0f)
+        val numPeople = intent.getIntExtra("numPeople", 0)
+        val percentage = intent.getIntExtra("percentage", 0)
+        val totalAmount = intent.getFloatExtra("totalAmount", 0.0f)
+
+        println("Euclides1 " + totalAmount)
+
+
     }
 }
